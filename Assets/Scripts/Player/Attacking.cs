b60237d8a17b93dcc440ec3131414cd06930d8fb
Qarian <Ferno>
2019-09-13 23:03:42 +0200

    My first commit

---
+++ Assets/Scripts/Player/Attacking.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Attacking : MonoBehaviour
+{
+	[SerializeField]
+	float cooldown = 3f;
+
+	[Space]
+	[SerializeField]
+	KeyCode keyAttack = KeyCode.Mouse0;
+
+	float lastAttack = 0f;
+
+
+    void Update()
+    {
+		if (Input.GetKeyDown(keyAttack))
+			Attack();
+    }
+
+	void Attack()
+	{
+
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField]
+	float speed;
+
+	Rigidbody2D rb;
+
+	Vector2 inputDir;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		inputDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+	}
+
+	private void FixedUpdate()
+	{
+		Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+		rb.MovePosition(pos + inputDir * speed * Time.fixedDeltaTime);
+	}
+}
