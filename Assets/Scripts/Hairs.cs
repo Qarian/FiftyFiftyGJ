02c925d65fec9177a43d58b0695bc2e5e21a87d8
Qarian <Ferno>
2019-09-14 02:00:22 +0200

    simpleHair

---
+++ Assets/Scripts/Hairs.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hairs : MonoBehaviour
+{
+	LineRenderer lineRenderer;
+
+	int points = 8;
+
+	[SerializeField]
+	List<Transform> nodes;
+
+	private void Start()
+	{
+		lineRenderer = GetComponent<LineRenderer>();
+	}
+
+	private void Update()
+	{
+		for (int i = 0; i < points; i++)
+		{
+			lineRenderer.SetPosition(i, nodes[i].position);
+		}
+	}
+
+	public void Attack()
+	{
+
+	}
+}
--- Assets/Scripts/Player/Attacking.cs
+++ Assets/Scripts/Player/Attacking.cs
@@ -5,21 +5,27 @@ public class Attacking : MonoBehaviour
 	[SerializeField]
 	float cooldown = 3f;
 
+	public Vector3 force;
+
 	[Space]
 	[SerializeField]
 	KeyCode keyAttack = KeyCode.Mouse0;
 
 	float lastAttack = 0f;
 
+	Hairs root;
 
-    void Update()
-    {
-		if (Input.GetKeyDown(keyAttack))
-			Attack();
-    }
-
-	void Attack()
+	private void Start()
 	{
-
+		root = GetComponent<Hairs>();
 	}
+
+	void Update()
+    {
+		if (Input.GetKeyDown(keyAttack) && Time.time > lastAttack + cooldown)
+		{
+			lastAttack = Time.time;
+			root.Attack();
+		}
+    }
 }
