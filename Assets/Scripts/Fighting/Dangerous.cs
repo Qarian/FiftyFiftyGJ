75354ece033dc802171c83d6a2ff5638f1180a83
Qarian <Ferno>
2019-09-15 05:46:25 +0200

    Enemy

---
+++ Assets/Scripts/Fighting/Dangerous.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Dangerous : MonoBehaviour
+{
+	AudioSource sound;
+
+	float lastCollision = 0f;
+
+	private void Start()
+	{
+		sound = GetComponent<AudioSource>();
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (lastCollision + 0.5f < Time.time)
+		{
+			collision.transform.parent.GetComponent<Hairs>().RemoveNode();
+
+			lastCollision = Time.time;
+			sound.Play();
+		}
+	}
+}
